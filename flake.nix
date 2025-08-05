ae2f43b7a18b491b5a5722b91a7fa915d0b14e92
connerohnesorge <conneroisu@outlook.com>
2025-08-05 05:27:28 -0500

    fix sync ability by adding GIO_MODULE_DIR

--- flake.nix
+++ flake.nix
@@ -29,14 +29,12 @@
             url = "https://proton.me/download/authenticator/linux/ProtonAuthenticator_${version}_amd64.deb";
             sha256 = "sha256-Ri6U7tuQa5nde4vjagQKffWgGXbZtANNmeph1X6PFuM=";
           };
-
           nativeBuildInputs = with pkgs; [
             dpkg
             autoPatchelfHook
             wrapGAppsHook3
             makeWrapper
           ];
-
           buildInputs = with pkgs; [
             gtk3
             glib
@@ -46,6 +44,10 @@
             atk
             webkitgtk_4_1
             libsoup_3
+            gnutls
+            openssl
+            cacert
+            glib-networking
           ];
 
           unpackPhase = ''
@@ -53,31 +55,38 @@
             dpkg-deb -x $src .
             runHook postUnpack
           '';
-
           installPhase = ''
             runHook preInstall
-            
+
             mkdir -p $out/bin
             mkdir -p $out/share/applications
             mkdir -p $out/share/icons/hicolor
-            
+
             # Copy the binary
             install -m755 usr/bin/proton-authenticator $out/bin/
-            
+
             # Copy desktop file
             cp "usr/share/applications/Proton Authenticator.desktop" $out/share/applications/proton-authenticator.desktop
-            
+
             # Fix desktop file to use correct binary name
             substituteInPlace $out/share/applications/proton-authenticator.desktop \
               --replace-fail "Exec=proton-authenticator" "Exec=$out/bin/proton-authenticator" \
               --replace-fail "Icon=proton-authenticator" "Icon=proton-authenticator"
-            
+
             # Copy icons
             cp -r usr/share/icons/hicolor/* $out/share/icons/hicolor/
-            
+
             runHook postInstall
           '';
-
+          # Set runtime environment variables for TLS support and GIO modules
+          preFixup = ''
+            gappsWrapperArgs+=(
+              --set SSL_CERT_FILE "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              --set CURL_CA_BUNDLE "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              --set NIX_SSL_CERT_FILE "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              --set GIO_MODULE_DIR "${pkgs.glib-networking}/lib/gio/modules"
+            )
+          '';
           meta = with pkgs.lib; {
             description = "Proton Authenticator - secure 2FA app";
             homepage = "https://proton.me/support/authenticator";
@@ -86,19 +95,21 @@
             maintainers = with maintainers; [connerohnesorge];
           };
         };
-
         proton-authenticator = self.packages.${system}.default;
       };
-
       # Development shell for testing and debugging
       devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
+        packages = with pkgs; [
+          alejandra
+          nixd
           dpkg
           file
           patchelf
         ];
+        # env = {
+        #   GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules";
+        # };
       };
-
       # Formatter configuration
       formatter = treefmtEval.config.build.wrapper;
       checks = {
